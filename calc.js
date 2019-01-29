c16420e53e06187635cf42f64555a7778100e73f
Stephen <stephen.nardone2@gmail.com>
2019-01-28 22:32:28 -0500

    Html and CSS functional. TODO finish JS

---
+++ calc.js
@@ -0,0 +1,24 @@
+// Operator Functions
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+function subtract(n1, n2) {
+    return n1 - n2;
+}
+
+function multiply() {
+    return n1 * n2;
+}
+
+function divide(){
+    if (n1 === 0 || n2 === 0){
+        return "That's impossible, but nice try.";
+    } else {
+        return n1 / n2;
+    }
+}
+
+function calculate(n1, operator, n2) {
+
+}
